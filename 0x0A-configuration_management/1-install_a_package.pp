781c9fefe3d0cba28c08106bec7bcdc8c4fd85cd
maqamylee0 <emmillynamuganga@gmail.com>
2023-11-25 12:04:50 +0300

    add prerequisites

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,12 @@
+package { 'python3-pip':
+  ensure   => 'present',
+}
+package { 'werkzueg':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
 package { 'flask':
   ensure   => '2.1.0',
+  require  => Package['werkzeug'],
   provider => 'pip3',
 }
