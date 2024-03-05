f1cd090b37b4f4e7b87b6b1f765066f231957b97
maqamylee0 <maqamylee0@gmail.com>
2024-03-06 00:45:41 +0300

    limit posts

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,15 @@ subredit = sys.argv[1]
 
 def number_of_subscribers(subreddit):
     """Get number of subscribers for a given subreddit"""
-    if subreddit is None:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+
+        return response.json().get('data').get('subscribers')
+    except Exception:
         return 0
-    return response.json().get('data').get('subscribers')
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,19 +9,27 @@ subredit = sys.argv[1]
 
 def recurse(subreddit, hot_list=[], after=None):
     """Get top allhot posts titles for a given subreddit recursively"""
-    if subreddit is None:
+    if subreddit is None or not isinstance(subreddit, str):
         return None
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=1'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if after:
         url += '?after={}'.format(after)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    params = {'limit': 100,
+              after: after}
+    try:
+        response = requests.get(url, headers=headers, 
+                                params=params,
+                                allow_redirects=False)
+        
+        if response.status_code != 200:
+            return None
+        data = response.json().get('data')
+        hot_list.extend([post.get('data').get('title')
+                        for post in data.get('children')])
+        after = data.get('after')
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        return hot_list
+    except Exception:
         return None
-    data = response.json().get('data')
-    hot_list.extend([post.get('data').get('title')
-                     for post in data.get('children')])
-    after = data.get('after')
-    if after is not None:
-        return recurse(subreddit, hot_list, after)
-    return hot_list
