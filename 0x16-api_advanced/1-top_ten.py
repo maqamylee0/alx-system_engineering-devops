441720d1dfc9ff6858fe9d286fde642c0cf0d932
maqamylee0 <maqamylee0@gmail.com>
2024-03-05 23:57:50 +0300

    use listings to limit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Query reddit api top ten- posts titles for a subredit"""
+
+import requests
+from os import sys
+
+subredit = sys.argv[1]
+def top_ten(subreddit):
+    """Get top 10 hot posts titles for a given subreddit"""
+    if subreddit is None:
+        return 0
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    posts = response.json().get('data').get('children')
+    for post in posts:
+        print(post.get('data').get("title"))
