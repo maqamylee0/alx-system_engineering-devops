fa6af8e894ead50d7254cbe2f3f24c0b9d1d4488
maqamylee0 <maqamylee0@gmail.com>
2024-03-05 23:47:24 +0300

    get subscriber count

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Query reddit api subscribers for a subredit"""
+
+import requests
+from os import sys
+
+subredit = sys.argv[1]
+def number_of_subscribers(subreddit):
+    """Get number of subscribers for a given subreddit"""
+    if subreddit is None:
+        return 0
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')
