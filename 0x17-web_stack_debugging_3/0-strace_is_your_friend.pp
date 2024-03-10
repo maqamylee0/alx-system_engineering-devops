26ac3f634eb105cc8ef7e2deee3b611593b6b32e
maqamylee0 <maqamylee0@gmail.com>
2024-03-10 11:24:22 +0300

    puppet format

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -5,8 +5,8 @@ $replace_string = 'php'
 
 # Execute sed command to replace strings
 exec { 'edit_file':
-  command => "/bin/sed -i 's/${search_string}/${replace_string}/g' ${config_file}",
-  path    => ['/bin','/usr/bin'],
-  onlyif  => "test -f ${config_file}",  # Check if file exists before executing
+  command   => "/bin/sed -i 's/${search_string}/${replace_string}/g' ${config_file}",
+  path      => ['/bin','/usr/bin'],
+  onlyif    => "test -f ${config_file}",  # Check if file exists before executing
   logoutput => true,  # Log command output for debugging
 }
